30fcdd528862c505c9be5a10faefc54ac9049337
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2025-03-05 18:42:55 +0300

    pre-test app

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,46 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_ar/my_home_page.dart';
+import 'package:augmented_reality_plugin/augmented_reality_plugin.dart';
+import 'package:camera/camera.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    var allCameras = await availableCameras();
+  } on CameraException catch (errorMessage) {
+    if (kDebugMode) {
+      print('Error: ${errorMessage.description}');
+    }
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      title: 'Augmented Reality App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const AugmentedRealityView(),
+    );
+  }
+}
+
+class AugmentedRealityView extends StatefulWidget {
+  const AugmentedRealityView({super.key});
+  @override
+  _AugmentedRealityViewState createState() => _AugmentedRealityViewState();
+}
+
+class _AugmentedRealityViewState extends State<AugmentedRealityView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Augmented Reality View')),
+      body: AugmentedRealityPlugin(
+        'https://www.freepnglogos.com/uploads/furniture-png/furniture-png-transparent-furniture-images-pluspng-15.png',
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
